172e4e4f0701178aa20ee91644c3ee428f0dbc47
Stenodyon <stenodyon@gmail.com>
2019-11-22 15:45:12 +0100

    removed code duplication and off by one

--- src/gui_helpers.rs
+++ src/gui_helpers.rs
@@ -1,7 +1,34 @@
 use super::{codepage437::to_cp437, Console, RGB};
 
-/// Draws a box, starting at x/y with the extents width/height using CP437 line characters
-pub fn draw_box(
+struct BoxDef {
+    hor: u8,
+    vert: u8,
+    top_left: u8,
+    top_right: u8,
+    bottom_left: u8,
+    bottom_right: u8,
+}
+
+const BOX_DEF_SINGLE: BoxDef = BoxDef {
+    hor: 196,
+    vert: 179,
+    top_left: 218,
+    top_right: 191,
+    bottom_left: 192,
+    bottom_right: 217,
+};
+
+const BOX_DEF_DOUBLE: BoxDef = BoxDef {
+    hor: 205,
+    vert: 186,
+    top_left: 201,
+    top_right: 187,
+    bottom_left: 200,
+    bottom_right: 188,
+};
+
+#[inline]
+fn _draw_box(
     console: &mut dyn Console,
     sx: i32,
     sy: i32,
@@ -9,6 +36,7 @@ pub fn draw_box(
     height: i32,
     fg: RGB,
     bg: RGB,
+    box_def: BoxDef,
 ) {
     for y in sy..sy + height {
         for x in sx..sx + width {
@@ -22,18 +50,37 @@ pub fn draw_box(
         }
     }
 
-    console.set(sx, sy, fg, bg, to_cp437('┌'));
-    console.set(sx + width, sy, fg, bg, to_cp437('┐'));
-    console.set(sx, sy + height, fg, bg, to_cp437('└'));
-    console.set(sx + width, sy + height, fg, bg, to_cp437('┘'));
+    console.set(sx, sy, fg, bg, box_def.top_left);
+    console.set(sx + width - 1, sy, fg, bg, box_def.top_right);
+    console.set(sx, sy + height - 1, fg, bg, box_def.bottom_left);
+    console.set(
+        sx + width - 1,
+        sy + height - 1,
+        fg,
+        bg,
+        box_def.bottom_right,
+    );
     for x in sx + 1..sx + width {
-        console.set(x, sy, fg, bg, to_cp437('─'));
-        console.set(x, sy + height, fg, bg, to_cp437('─'));
+        console.set(x, sy, fg, bg, box_def.hor);
+        console.set(x, sy + height - 1, fg, bg, box_def.hor);
     }
     for y in sy + 1..sy + height {
-        console.set(sx, y, fg, bg, to_cp437('│'));
-        console.set(sx + width, y, fg, bg, to_cp437('│'));
+        console.set(sx, y, fg, bg, box_def.vert);
+        console.set(sx + width - 1, y, fg, bg, box_def.vert);
     }
+}
+
+/// Draws a box, starting at x/y with the extents width/height using CP437 line characters
+pub fn draw_box(
+    console: &mut dyn Console,
+    sx: i32,
+    sy: i32,
+    width: i32,
+    height: i32,
+    fg: RGB,
+    bg: RGB,
+) {
+    _draw_box(console, sx, sy, width, height, fg, bg, BOX_DEF_SINGLE);
 }
 
 /// Draws a box, starting at x/y with the extents width/height using CP437 line characters
@@ -46,30 +93,7 @@ pub fn draw_box_double(
     fg: RGB,
     bg: RGB,
 ) {
-    for y in sy..sy + height {
-        for x in sx..sx + width {
-            console.set(
-                x,
-                y,
-                RGB::from_f32(1.0, 1.0, 1.0),
-                RGB::from_f32(0.0, 0.0, 0.0),
-                32,
-            );
-        }
-    }
-
-    console.set(sx, sy, fg, bg, to_cp437('╔'));
-    console.set(sx + width, sy, fg, bg, to_cp437('╗'));
-    console.set(sx, sy + height, fg, bg, to_cp437('╚'));
-    console.set(sx + width, sy + height, fg, bg, to_cp437('╝'));
-    for x in sx + 1..sx + width {
-        console.set(x, sy, fg, bg, to_cp437('═'));
-        console.set(x, sy + height, fg, bg, to_cp437('═'));
-    }
-    for y in sy + 1..sy + height {
-        console.set(sx, y, fg, bg, to_cp437('║'));
-        console.set(sx + width, y, fg, bg, to_cp437('║'));
-    }
+    _draw_box(console, sx, sy, width, height, fg, bg, BOX_DEF_DOUBLE);
 }
 
 /// Draws a horizontal progress bar
